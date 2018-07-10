4378a54f6401e0a652e003e7e7d2cda530f8085e
Elinx <654753115@qq.com>
2018-07-10 15:01:44 +0800

    增加线性算法

---
+++ tests/test006.py
@@ -0,0 +1,17 @@
+# 矩阵 - 张量
+
+import os
+import tensorflow as tf
+
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+
+with tf.Graph().as_default():
+    primes = tf.constant([2, 3, 5, 67, 11, 13], dtype=tf.int32)
+
+    ones = tf.ones([6], dtype=tf.int32)
+
+    just_beyond_primes = tf.multiply(primes, ones)
+
+    with tf.Session() as sess:
+        print(just_beyond_primes.eval())
+        print(sess.run(ones))
