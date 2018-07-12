a2875255a43c556103f0cd266d5a858c45d61ce9
Elinx <654753115@qq.com>
2018-07-12 18:04:18 +0800

    类学习

--- tests/libs/TestClass.py
+++ tests/libs/TestClass.py
@@ -60,4 +60,8 @@ class student(people):
     def speak(self):
         print("%s 说: 我 %d 岁了，我在读 %d 年级" % (self.name, self.age, self.grade))
 
+    """ 类静态方法定义 """
+    @staticmethod
+    def f():
+        print('runoob')
 
